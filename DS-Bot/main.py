f7429cee2117f9dc33b6e37bbc40fbd06685f404
Theotrgl <Justintheofilus@gmail.com>
2023-11-02 10:04:40 +0700

    changed role picking feature to utilize emoji's

--- DS-Bot/main.py
+++ DS-Bot/main.py
@@ -9,11 +9,26 @@ from apikeys import BOT_TOKEN
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 MAX_ROLES_PER_MEMBER = 1
+role_message_id = None
 
 @client.event
 async def on_ready():
+    global role_message_id
     print("Bot is Online...")
     print("...................")
+
+    # Fetch the channel where you want to send the role selection message
+    channel = client.get_channel(1168815727516590081)
+
+    role_message = await channel.send("React to assign yourself a role!\n\n"
+                                      "Artist - :artist:\n"
+                                      "Curator - :curator:")
+
+    # Add reactions to the message
+    await role_message.add_reaction("🎨")  # Artist emoji
+    await role_message.add_reaction("🔍")  # Curator emoji
+
+    role_message_id = role_message.id 
 
 # Hello Command
 @client.command()
@@ -57,66 +72,51 @@ async def ban_error(ctx, error):
         await ctx.send("You don't have permission to ban people!")
 
 ##ROLE PICKING SYSTEM
-class RoleButton(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.add_item(discord.ui.Button(label="Artist", custom_id="Artist", style=discord.ButtonStyle.red))
-        self.add_item(discord.ui.Button(label="Curator", custom_id="Pending", style=discord.ButtonStyle.blurple))
+# Load selected roles from JSON file
+def load_selected_roles():
+    try:
+        with open("selected_roles.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
 
-@client.command()
-async def role_pick(ctx):
-    view = RoleButton()
-    message = await ctx.send('Please choose the role you are applying as:', view=view, ephemeral = True)
+# Save selected roles to JSON file
+def save_selected_roles(selected_roles):
+    with open('selected_roles.json', 'w') as file:
+        json.dump(selected_roles, file)
+
+
+# Load the selected roles from the JSON file
+selected_roles = load_selected_roles()
 
 @client.event
-async def on_interaction(interaction):
-    if isinstance(interaction, discord.Interaction):
-        if interaction.data['custom_id'] == 'Artist':
-            await setRole(interaction)
-            await remove_buttons(interaction)
-        elif interaction.data['custom_id'] == 'Pending':
-            await setRole(interaction)  # Set the "Pending" role
-            await remove_buttons(interaction)
+async def on_raw_reaction_add(payload):
+    global role_message_id
+    channel = client.get_channel(payload.channel_id)
+    if channel.id == 1168815727516590081:  # Check if the reaction is in the specific channel
+        guild = client.get_guild(payload.guild_id)
+        member = guild.get_member(payload.user_id)
+        user_id = str(member.id)
 
-async def setRole(interaction):
-    role_name = interaction.data['custom_id']
-    member = interaction.user
-    guild = interaction.guild
-    mods = guild.get_role(1168399823205052436)
-    roles = member.roles[1:]  # Exclude @everyone role
+        if user_id in selected_roles:
+            # User has already selected a role, prevent any additional changes
+            return
 
-    if role_name == 'Pending':
-        # Notify the user
-        await interaction.response.send_message(f"Applying for Curator role requires additional steps.\n\nPlease submit the following information to the [] text-channel within at least 24 hours.\n\nThank You", ephemeral=True)
+        if payload.message_id == role_message_id:
+            if str(payload.emoji) == '🎨':  # Artist emoji
+                role = discord.utils.get(guild.roles, name="Artist")
+            elif str(payload.emoji) == '🔍':  # Curator emoji
+                role = discord.utils.get(guild.roles, name="Pending")
 
-        # Notify moderators in a separate channel and tag them
-        mod_channel = guild.get_channel(1168806364697608303)
-        if mod_channel:
-            await mod_channel.send(f"{member.mention} applied for the Curator role. Please review. {mods.mention}")
+            if role:
+                await member.add_roles(role)
+                await member.send(f"You've been assigned the {role.name} role.")
 
-        return
+                selected_roles[user_id] = role.name  # Store the user and their selected role
+                save_selected_roles(selected_roles)  # Save the updated data to the JSON file
+            else:
+                print("Role not found.")  # Handle role not found scenario
 
-    # For other roles
-    role = discord.utils.get(guild.roles, name=role_name)
-    if role:
-        if role in roles:
-            await interaction.response.send_message(f"You already have the {role_name} role.", ephemeral=True)
-        else:
-            await member.add_roles(role)
-            await interaction.response.send_message(f"You've been assigned the {role_name} role.", ephemeral=True)
-    else:
-        await interaction.response.send_message(f"Role '{role_name}' not found.", ephemeral=True)
-
-
-async def remove_buttons(interaction):
-    # Fetch the message to update and remove the buttons
-    message = await interaction.message.channel.fetch_message(interaction.message.id)
-    await message.edit(view=None)
-
-@role_pick.error
-async def role_pick_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You don't have permissions to use this command.")
 
 #ROLE ADDING AND REMOVING FOR MODS
 @client.command()
@@ -128,20 +128,46 @@ async def addRole(ctx, user: discord.Member, *, role: discord.Role):
         await user.add_roles(role)
         await ctx.send(f"Added {role} to {user.mention}.")
 
+        # Update selected_roles data
+        user_id = str(user.id)
+        if user_id not in selected_roles:
+            selected_roles[user_id] = []
+
+        selected_roles[user_id].append(role.name)
+        save_selected_roles(selected_roles)
+
 @addRole.error
-async def role_error(ctx, error):
+async def addRole_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("You do not have permission to use this command.")
 
 @client.command()
 @has_permissions(manage_roles=True)
 async def removeRole(ctx, user: discord.Member, *, role: discord.Role):
+    print("test 1")
+    user_id = str(user.id)
+    
     if role in user.roles:
+        print("test 2")
         await user.remove_roles(role)
         await ctx.send(f"Removed {role} from {user.mention}.")
+        
+        print("test 3")
+
+        # Update selected_roles data
+        if user_id in selected_roles and role.name in selected_roles[user_id]:
+            print("test 4")
+            del selected_roles[user_id]
+            print(f"Role {role.name} removed from {user_id} in the JSON file.")
+            save_selected_roles(selected_roles)
+        else:
+            await ctx.send(f"Role {role.name} not found in selected_roles for {user_id}.")
     else:
         await ctx.send(f"{user.mention} does not have the role {role}.")
 
+
+
+        
 @removeRole.error
 async def removeRole_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
